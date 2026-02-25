7787f30888dc218668f29b16fb21b5471f0064b0
Rajdeep Rudra <rajdeeprudra2003@gmail.com>
2026-02-26 02:09:56 +0530

    Added Prisma Schema for the DB

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -3,6 +3,5 @@ import express from "express";
 const app = express();
 
 
-// postgres string
 
-//postgresql://neondb_owner:npg_PpgWvHOk9Iw1@ep-holy-sky-ai35v887-pooler.c-4.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require
+
